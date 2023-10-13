98532108de166bf238dfaa0055801662acff7ef4
xy-wu <wxy@g.ecc.u-tokyo.ac.jp>
2023-10-13 18:43:45 +0900

    demo

--- src/demos/firework.py
+++ src/demos/firework.py
@@ -1,5 +1,16 @@
 from ballistic import Shot
 from src.PyGameEngine import Particle
+import numpy as np
+
+
+GRAVITY = np.array([0.0, -9.8, 0.0], dtype="f4")    # earth gravity
+global GRAVITY
+
+
+class Payload:
+    def __init__(self, type, count):
+        self.type = type
+        self.count = count
 
 
 class FireworkRule:
@@ -11,25 +22,23 @@ class FireworkRule:
         self.maxVelocity = velocityRange[1]
         self.damping = damping
 
-        class Payload:
-            type = None
-            count = 0
+        self.payloadCount = 0
+        self.payload = Payload(type, self.payloadCount)
 
-        self.payload = Payload()
+    def create(self, firework: Particle, parent:Particle=None):
+        r = np.random.randint(0, 100)
+        firework.type = self.type
+        firework.age = np.random.randint(self.minAge, self.maxAge)
+        if parent:
+            firework.setPosition(parent.position)
+        vel = parent.velocity
+        vel += np.random.randint(self.minVelocity, self.maxVelocity)
+        firework.setVelocity(vel)
 
-    def create(self):
-        # result = Shot(self.type)
-        # result.particle.setMass(1.0)
-        # result.particle.setDamping(self.damping)
-        # result.particle.setPosition((0., 0., 0.))
-        # result.particle.setVelocity((0., self.minVelocity + (self.maxVelocity - self.minVelocity) * random.random(), 0.))
-        # result.particle.setAcceleration((0., 0.5, 0.))
-        # result.particle.clearAccumulator()
-        # result.particle.setMass(1.0)
-        # return result
-        pass
-
-
+        firework.setMass(1.0)
+        firework.setDamping(self.damping)
+        firework.setAcceleration(GRAVITY)
+        firework.clearAccumulator()
 
 
 class Firework(Particle):
@@ -38,8 +47,43 @@ class Firework(Particle):
         self.age = 0.0
 
 
-    def update(self, duration):
+    def update(self, duration) -> bool:
         self.integrate(duration)
         self.age -= duration
         return self.age < 0.0
+
+
+class FireworkDemo:
+    def initFireworkRules(self, firework: Particle):
+        fireworkRules = FireworkRule(
+            "firework",
+            (2, 5),
+            (10, 20),
+            0.9)
+
+        fireworkRules.create(firework)
+
+    def create(self, type, parent: Particle=None):
+        firework = Firework()
+        self.initFireworkRules(firework)
+        return firework
+
+
+
+
+if __name__ == "__main__":
+    maxFireworks = 100
+    duration = 0.1
+    for i in range(maxFireworks):
+        firework = Firework()
+        while(firework.type > 0 and firework.update(duration)):
+            rule = FireworkRule()
+            rule = rules + firework.type - 1
+
+            firework.type = 0
+
+
+        fireworkDemo = FireworkDemo()
+        fireworkDemo.initFireworkRules(firework)
+        print("firework: ", firework.type, firework.age, firework.velocity, firework.position)
 
