14f7218b657cd71fb94f6f5ca275a769db3c411b
xy-wu <wu.xuanyu@hotmail.com>
2024-08-11 15:52:47 +0900

    Update: separate create_buffer and draw call

--- pymrt/Renderer/particle_system.py
+++ pymrt/Renderer/particle_system.py
@@ -1,5 +1,6 @@
 import moderngl
 import numpy as np
+import torch
 
 from .system_base import SystemBase
 
@@ -8,46 +9,39 @@ class ParticleSystem(SystemBase):
     def __init__(self, ctx):
         super().__init__(ctx, "./shaders/particle.glsl")
         self.point_size = 10
-        self.use_circle = True
+        self.rect_mode = False
 
+        self.particles = []
         self.vao, self.vbo, self.ebo = None, None, None
 
-    def set_point_size(self, size):
-        self.point_size = size
-        self.set_uniform("point_size", self.point_size.to_bytes(4, "little"))
+    # def set_point_size(self, size):
+    #     self.point_size = size
+    #     self.set_uniform("point_size", self.point_size.to_bytes(4, "little"))
 
-    def set_use_circle(self, use_circle):
-        self.use_circle = use_circle
-        self.set_uniform("use_circle", self.use_circle.to_bytes(4, "little"))
+    def set_to_rect_mode(self, rect_mode):
+        self.rect_mode = rect_mode
+        self.set_uniform("rectMode", self.rect_mode.to_bytes(4, "little"))
 
-    def draw(self, vertices=None, indices=None, point_size=40, use_circle=True):
-        self._program["point_size"].value = self.point_size
-        self._program["use_circle"].value = self.use_circle
-        vbo = self.ctx.buffer(np.array(vertices, dtype="f4"))
+    def create_buffer(
+        self,
+        pos: list | np.ndarray | torch.Tensor = None,
+        color=[0.0, 0.0, 0.0],
+        point_size: float = 6,
+    ):
+        for i in range(len(pos)):
+            self.particles.append((*pos[i], *color, point_size))
 
-        has_indices = indices is not None
-        if has_indices:
-            ebo = self.ctx.buffer(np.array(indices, dtype="i4"))
-            vao_content = [
-                (vbo, "2f", 0),
-                (ebo, "1i", 1),
-            ]
-        else:
-            vao_content = [
-                (vbo, "2f", 0),
-                # (indices_buffer, "1i /r", 1), # maybe specify a color
-            ]
+    def setup(self):
+        self.set_uniform("rectMode", self.rect_mode.to_bytes(4, "little"))
+        self.vbo = self.ctx.buffer(np.array(self.particles, dtype="f4"))
 
-        vao = self.ctx.vertex_array(self._program, vao_content)
-        vao.render(moderngl.POINTS)
+        vao_content = [(self.vbo, "2f 3f 1f", 0, 1, 2)]
 
-        if has_indices:
-            self._release(vao, vbo, ebo)
-        else:
-            self._release(vao, vbo)
+        self.vao = self.ctx.vertex_array(self._program, vao_content)
 
-    # def draw(self):
-    #     self.vao = self.ctx.vertex_array(self._program, vao_content)
-    #     self.vao.render(moderngl.POINTS)
-    #
-    #     self._release(self.vao, self.vbo, self.ebo)
+    def draw(self):
+        self.setup()
+        self.vao.render(moderngl.POINTS)
+        # clean up
+        self.particles.clear()
+        self._release(self.vao, self.vbo, self.ebo)
--- pymrt/Renderer/shaders/particle.glsl
+++ pymrt/Renderer/shaders/particle.glsl
@@ -7,51 +7,52 @@
 #define INV_PI 0.31830988618379067153776752674503
 
 // https://stackoverflow.com/questions/4200224/random-noise-functions-for-glsl
-float rand(vec2 co){
-    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
-}
-
-vec3 rand3(vec2 co){
-    return vec3(rand(co), rand(co + 1.0), rand(co - 1.0));
-}
+//float rand(vec2 co){
+//    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+//}
+//
+//vec3 rand3(vec2 co){
+//    return vec3(rand(co), rand(co + 1.0), rand(co - 1.0));
+//}
 
 #if defined VERTEX_SHADER
 
-layout (location = 0) in vec2 vert;
-layout (location = 1) in int vert_id;
+layout (location = 0) in vec2 pos;
+layout (location = 1) in vec3 color;
+layout (location = 2) in float size;
 
-uniform float point_size = 1.0;
-uniform mat3 transform = mat3(1.0);
+//uniform float point_size = 1.0;
+uniform mat3 transform3 = mat3(1.0);
 
-out vec3 frag_color;
+out vec3 fragColor;
 
 void main() {
-    frag_color = rand3(vec2(vert_id, 0.0));
-    vec3 pos = transform * vec3(vert, 1.0);
-    gl_Position = vec4(pos.xy, 0.0, 1.0);
-    gl_PointSize = point_size;
+//    frag_color = rand3(vec2(iVert, 0.0));
+    fragColor = color;
+    vec3 fixedPos = transform3 * vec3(pos, 1.0);
+    gl_Position = vec4(fixedPos.xy, 0.0, 1.0);
+    gl_PointSize = size;
 }
 
 #elif defined FRAGMENT_SHADER
 
-in vec3 frag_color;
+in vec3 fragColor;
 
-out vec4 out_color;
+out vec4 outColor;
 
-uniform bool use_circle = false;
+uniform bool rectMode = false;
 
 void main() {
     //    float dist = smoothstep(0.0, 0.5,
     //        distance(gl_PointCoord.xy, vec2(0.5)));
     //    out_color = vec4(frag_color, cos(dist * HALF_PI));
     // out_color = vec4(frag_color, 1.0 - dist * dist);
-    if (use_circle) {
+    if (!rectMode) {    // circle mode
         float dist = step(0.5, distance(gl_PointCoord.xy, vec2(0.5)));
-        out_color = vec4(frag_color, 1.0 - dist);
+        outColor = vec4(fragColor, 1.0 - dist);
         return;
     }
-
-    out_color = vec4(frag_color, 1.0);
+    outColor = vec4(fragColor, 1.0);
 }
 
 #endif
